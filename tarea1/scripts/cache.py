7988d3d01796e8195c849f92beec91e3d0fb44b8
daniel garcia vaglio <degv364@gmail.com>
2017-03-24 13:30:14 -0600

    Add cache structure

--- tarea1/scripts/cache.py
+++ tarea1/scripts/cache.py
@@ -42,12 +42,57 @@ from utils import log2, log16, int2hex, int2bin, hex2bin, hex2bin, bin2hex
         for i in [0,1,2,3]:
             self.info[int(offset, 2)+i]=data[i]
 
+class Cache1w():
+    #data sizr in bytes, block size in bytes, asociativity is a bool
+    #note that there is no need for shared, since this cache is alone
+    #bus side requests, onlyflush has effect, the rest depend on havin more than one cache
+    def __init__(self, data_size, block_size,param_dicc={}):
+
+        self.cmd_from_cache=param_dicc["cmd_from_cache"]
+        self.data_from_cache=param_dicc["data_from_cache"]
+        self.data_from_mem=param_dicc["data_from_mem"]
+        self.cmd_to_mem=param_dicc["cmd_to_mem"]
+        self.data_to_mem=param_dicc["data_to_mem"]
+        self.data_to_cache=param_dicc["data_to_cache"]
+        
+        index_cant=data_size/block_size;
+        self.index_size=int(log2(index_cant));
+        self.offset_size=int(log2(block_size))
+        self.data={}
+        self.instruction=None
+        for index in xrange(index_cant):
+            self.data[int2bin(index, self.index_size)]=Block1w()
+
+    def split_instruction(self, instruction):
+        #instruction is a list, fisrt element is adress, return a list with tag, index and offset
+        #instruction coems in hex
+        address=instruction[0]
+        ins=hex2bin(address.split('x')[1])
+        offset=ins[-self.offset_size:]
+        index=ins[(-self.index_size-self.offset_size):-self.offset_size]
+        tag=ins[:(-self.index_size-self.offset_size)]
+        return [tag,index,offset]
+
+
+    def execution_loop(self):
+        while (True):
+            if not (self.cmd_from_cache.poll():
+                sleep(1/1000)
+            else:
+                #there is a command from cacheL1
+                instruction=self.cmd_from_cache.recv()
+                if self.data_from_cache.poll()
+                    data=self.data_from_cache.recv()
+                else:
+                    data=None
+                self.run_instruction(instruction, data) #FIXME: change implementation
+
 
 
 
 def cacheL2(param_dicc):
     cache=Cache1w(128000, 32,param_dicc)
-    cache.execution_loop() #FIXME: missing implementation
+    cache.execution_loop() 
 
     
 
