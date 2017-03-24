839221053d73c91512ad86b9d9de5f208efcad77
daniel garcia vaglio <degv364@gmail.com>
2017-03-24 12:48:52 -0600

    Main agregado, el esqueleto de lso modulos de entradas y salidas se define ahi

---
+++ tarea1/main.py
@@ -0,0 +1,90 @@
+from multiprocessing import Pipe, Process
+from parser import get_adresses
+from core import core
+from cache import cacheL1, cacheL2
+from mem import mem
+
+
+def main():
+    #get the instructions----------------------------------------------------------------------
+    instructions_list_core_1=get_addresses("instructions_core_1.txt")
+    instructions_list_core_2=get_addresses("instructions_core_2.txt")
+    
+    #create the connections --------------------------------------------------------------------
+    #data cacheL1->core
+    [data_core1_from_cache1, data_cache1_to_core1]=Pipe(False)
+    [data_core2_from_cache2, data_cache2_to_core2]=Pipe(False)
+    #cmmand core->cacheL1
+    [cmd_cache1_from_core1, cmd_core1_to_cache1]=Pipe(False)
+    [cmd_cache2_from_core2, cmd_core2_to_cache2]=Pipe(False)
+    #data core->cache1
+    [data_cache1_from_core1, data_core1_to_cache1]=Pipe(False)
+    [data_cache2_from_core2, data_core2_to_cache2]=Pipe(False)
+    
+    #cmd cacheL1->cacheL2
+    [cmd_L2_from_L1, cmd_L1_to_L2]=Pipe(False)
+    #data cacheL1->cacheL2
+    [data_L2_from_L1, data_L1_to_L2]=Pipe(False)
+    #data cacheL2->cacheL1
+    [data_L1_from_L2, data_L2_to_L1]=Pipe(False)
+    
+    #cmd cacheL2->Mem
+    [cmd_mem_from_cache, cmd_cache_to_mem]=Pipe(False)
+    #data cacheL2->Mem
+    [data_mem_from_cache, data_cache_to_mem]=Pipe(False)
+    #data mem->busL2
+    [data_cache_from_mem, data_mem_to_cache]=Pipe(False)
+
+    #parameters in diccionary------------------------------------------------------------------
+    core1_parameters={"instructions":instructions_list_core_1,
+                      "cmd_to_cache":cmd_core1_to_cache1,
+                      "data_from_cache":data_core1_from_cache1,
+                      "data_to_cache":  data_core1_to_cache1}
+    core2_parameters={"instructions":instructions_list_core_1,
+                      "cmd_to_cache": cmd_core2_to_cache2,
+                      "data_from_cache": data_core2_from_cache2,
+                      "data_to_cache":  data_core2_to_cache2} 
+
+    cacheL1_parameters={"cmd_from_core1":cmd_cache1_from_core1,
+                          "cmd_from_core2": cmd_cache2_from_core2,
+                          "data_from_core1": data_cache1_from_core1,
+                          "data_from_core2": data_cache2_from_core2,
+                          "cmd_to_cache": cmd_L1_to_L2,
+                          "data_to_cache": data_L1_to_L2,
+                          "data_from_cache":data_L1_from_L2}
+
+    cacheL2_parameters={"cmd_from_cache": cmd_L2_from_L1,
+                        "data_from_cache": data_L2_from_L1,
+                        "data_to_cache": data_L2_to_L1,
+                        "cmd_to_mem": cmd_cache_to_mem,
+                        "data_to_mem": data_cache_to_mem,
+                        "data_from_mem":data_cache_from_mem}
+
+    mem_parameters={"cmd_from_cache": cmd_mem_from_cache,
+                    "data_from_cache": data_mem_from_cache,
+                    "data_to_cache": data_mem_to_cache}
+
+    #create the processes-----------------------------------------------------
+    core1_p=Process(core, core1_parameters)
+    core2_p=Process(core, core2_parameters)
+    cacheL1_p=Process(cacheL1, cacheL1_parameters)
+    cacheL2_p=Process(cacheL2, cacheL2_parameters)
+    mem_p=Process(mem, mem_parameters)
+
+    #Process management--------------------------------------------------------------
+    core1_p.start()
+    core2_p.start()
+    cacheL1_p.start()
+    cacheL2_p.start()
+    mem_p.start()
+    core1_p.join()
+    core2_p.join()
+
+    cacheL1_p.terminate()
+    cacheL2_p.terminate()
+    mem_p.terminate()
+
+
+if __name__=="__main__":
+    main()
+
