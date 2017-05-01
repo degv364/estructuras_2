dc7a8742565dfb043034b250be6eeed75f02c465
daniel garcia vaglio <degv364@gmail.com>
2017-05-01 00:01:04 -0600

    Ad readme

--- tarea1/zipper.sh
+++ tarea1/zipper.sh
@@ -1,2 +1,2 @@
-zip B42781_B47769.zip doc/reporte.pdf scripts/block.py scripts/cacheL1.py scripts/cacheL2.py scripts/core.py scripts/main.py scripts/mem.py scripts/mem_trace_core2.txt scripts/mem_trace_core1.txt scripts/parser.py scripts/utils.py scripts/tests/test1.txt scripts/tests/text2.txt 
+zip B42781_B47769.zip doc/reporte.pdf scripts/block.py scripts/cacheL1.py scripts/cacheL2.py scripts/core.py scripts/main.py scripts/mem.py scripts/mem_trace_core2.txt scripts/mem_trace_core1.txt scripts/parser.py scripts/utils.py scripts/tests/test1.txt scripts/tests/test2.txt scripts/README
 
